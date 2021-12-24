cef51ef9d3670f6d45e8d816f9d205a4646363f2
vishalendu <vishalendu@gmail.com>
2021-12-24 10:19:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+### Twitter documentation : https://developer.twitter.com/en/docs/tutorials/explore-a-users-tweets
+
+TWITTER_BEARER = ''
+CONSUMER_KEY = ''
+CONSUMER_SEC = ''
+
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
+TWITTER_USERID = [
+    '52166809',
+    '906328017986899968',
+    '430841130',
+    '787439438964068352',
+    '2715646770'
+]
